38d8a41aa13afa0f12d875b70c275ca53d92cbc9
Jaynro <jaynro@gmail.com>
2024-07-27 13:20:58 -0600

    first Commit Leet code

---
+++ slidingWindow.py
@@ -0,0 +1,15 @@
+def find_length(nums, k):
+    # curr is the current sum of the window
+    left = curr = ans = 0
+    for right in range(len(nums)):
+        curr += nums[right]
+        while curr > k:
+            curr -= nums[left]
+            left += 1
+        ans = max(ans, right - left + 1)
+    
+    return ans
+
+k=8
+nums = [3,1,2,7,4,2,1,1,5]
+print(find_length(nums, k)) # 4
