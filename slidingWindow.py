5dcd50a4db18941be7c74705b36549830f39cbf5
Jaynro <jaynro@gmail.com>
2024-07-27 13:23:46 -0600

    Adding comments to sliding window.

--- slidingWindow.py
+++ slidingWindow.py
@@ -1,11 +1,16 @@
 def find_length(nums, k):
     # curr is the current sum of the window
     left = curr = ans = 0
+
+    # 1.Expand the window to the right
     for right in range(len(nums)):
         curr += nums[right]
+
+    # 2.Shrink the window from the left
         while curr > k:
             curr -= nums[left]
             left += 1
+    # 3.Update the result
         ans = max(ans, right - left + 1)
     
     return ans
